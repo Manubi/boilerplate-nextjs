5d9be6921604cebfb0b5b8168dd0c2e56151142b
Manuel Bichler <bichler@gmail.com>
2020-01-11 14:26:10 +0100

    first commit for boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] }
+    ],
+    "prettier/prettier": "error"
+  },
+  "prettier/prettier": ["error", { singleQuote: true }]
+};
